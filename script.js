40d09aed31c987a032ba508fa09bb1b54b66b794
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-12 12:45:18 -0500

    Add basic button functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let display = null;
+let firstInput = null;
+let secondInput = null;
+let operator = null;
+
 function add(a, b) {
   return a + b;
 }
@@ -14,9 +19,9 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
+function operate(operator, a, b = 0) {
   if (operator === '+') {
-    return add(a, b);
+    return add(+a, +b);
   }
   else if (operator === '−') {
     return subtract(a, b);
@@ -25,6 +30,64 @@ function operate(operator, a, b) {
     return multiply(a, b);
   }
   else if (operator === '÷') {
-    return divide(a, b);
+    if (b == 0) {
+      return 0;
+    }
+    else {
+      return divide(a, b);
+    }
   }
+}
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button) => {
+  button.addEventListener('click', operateButton);
+});
+
+function operateButton(button) {
+  if (button.target.classList.contains('number')) {
+    if (display == null) { // 'Blank' display
+      if (button.target.id == 'decimal') {
+
+      }
+      else {
+        display = button.target.textContent;
+      }
+    }
+    else if (button.target.id == 'decimal') {
+
+    }
+    else if (display.length < 6) {
+      display += button.target.textContent;
+    }
+    if (operator == null) {
+      firstInput = display;
+    }
+    else {
+      secondInput = display;
+    }
+    updateDisplay(display);
+  }
+  else if (button.target.classList.contains('operator')) {
+    if (button.target.id == 'equals') {
+      console.log('firstInput: ' + firstInput + ', secondInput: ' + secondInput + ', operator: ' + operator);
+      if (firstInput != null && operator != null) {
+        display = operate(operator, firstInput, secondInput);
+        console.log('Output: ' + display);
+        updateDisplay(display);
+        firstInput = display;
+        operator = null;
+        display = null;
+      }
+    }
+    else {
+      operator = button.target.textContent;
+      display = null;
+    }
+  }
+}
+
+function updateDisplay(display) {
+  const output = document.querySelector('.output');
+  output.textContent = Math.round(display * 100) / 100;
 }
